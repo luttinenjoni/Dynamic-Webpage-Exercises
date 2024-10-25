5d3e450e61aea7531c1fd646981bcdc899cfece6
luttinenjoni <luttinenjoni@gmail.com>
2024-10-25 16:21:58 +0300

    deleted files for open data excersice

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React, { useState } from 'react';
-import Header from './components/componentUI';
-import OrderInfo from './components/orderInfo';
-import OrderForm from './components/orderForm';
 import '/src/styles/ui.css'
 
 function App () {
-  const [selectedProduct, setSelectedProduct] = useState({ name: 'AMD Ryzen', price: 245 });
-  const [quantity, setQuantity] = useState(1);
+
 
   return (
     <div className="app">
-      <Header></Header>
-      <OrderForm onProductChange={setSelectedProduct} onQuantityChange={setQuantity} />
-      <OrderInfo product={selectedProduct} quantity={quantity} />
+
     </div>
   );
 };
+
+export default App;
--- src/components/componentUI.jsx
+++ src/components/componentUI.jsx
@@ -1,8 +1,7 @@
 function Header() {
  return (
     <div id="header">
-      <img src="/src/images/color-spectrum-1192509_1280.png" />
-      <h3>Welcome to the product page!</h3>
+
     </div>
   );
 }
