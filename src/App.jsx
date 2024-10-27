c9a2a7bae941e58fc64ee60fb5d3550094f99924
luttinenjoni <luttinenjoni@gmail.com>
2024-10-27 15:19:46 +0200

    Updated some text on the UI.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Pokémon Information</h1>
+      <p>Search for a pokémon and get a picture of it. For example try search for pikachu.</p>
       <PokemonInfo></PokemonInfo>
     </div>
   );
